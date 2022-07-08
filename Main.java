d8bdb76b621ec1bc661ae86ef0d997efe6c96898
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2022-07-07 23:58:31 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,9 @@ public class Main {
     int Str = 9;
     int Dex;
     int Con;
-
+    int Int;
+    int Wiz;
+    
     System.out.println("escrever:" + Str);
         double random = Math.random();
 }
